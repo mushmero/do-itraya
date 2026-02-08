ca64f7efaceacaea0b572107ae283805b9f1c2c1
amirul <hakim_s09@hotmail.com>
2026-02-08 16:55:25 +0800

    fix(config): change port to 3001

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -10,11 +10,11 @@ module.exports = {
       max_memory_restart: "512M",
       env: {
         NODE_ENV: "development",
-        PORT: 3000,
+        PORT: 3001,
       },
       env_production: {
         NODE_ENV: "production",
-        PORT: 3000,
+        PORT: 3001,
       },
       env_file: ".env",
     },
